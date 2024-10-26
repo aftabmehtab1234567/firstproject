e99e68ec1b58ca5fdc3917874b709375bbc43d5e
aftab alam <aftabalam9008@gmail.com>
2024-10-26 04:18:29 -0700

    update

--- src/test/java/Pageobject/loginpage.java
+++ src/test/java/Pageobject/loginpage.java
@@ -6,32 +6,36 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class loginpage {
-WebDriver ldriver;
-public loginpage(WebDriver rdriver) {
-	ldriver=rdriver;
-	PageFactory.initElements( rdriver,this);
-}
-@FindBy(name="user-name")
-WebElement txtUsername;
-@FindBy(name="password")
-WebElement txtPassword;
-@FindBy(name="login-button")
-WebElement btnLogin;
-@FindBy(id="add-to-cart-sauce-labs-backpack")
-WebElement productlink;
+	WebDriver ldriver;
 
-public void setusername(String uname) {
-	txtUsername.sendKeys(uname);
-}
-public void setPassword(String pass) {
-    txtPassword.sendKeys(pass);
-}
+	public loginpage(WebDriver rdriver) {
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
 
-public void clickLogin() {
-    btnLogin.click();
-}
-public void clickproductlink() {
-    productlink.click();
-}
+	@FindBy(name = "user-name")
+	WebElement txtUsername;
+	@FindBy(name = "password")
+	WebElement txtPassword;
+	@FindBy(name = "login-button")
+	WebElement btnLogin;
+	@FindBy(id = "add-to-cart-sauce-labs-backpack")
+	WebElement productlink;
+
+	public void setusername(String uname) {
+		txtUsername.sendKeys(uname);
+	}
+
+	public void setPassword(String pass) {
+		txtPassword.sendKeys(pass);
+	}
+
+	public void clickLogin() {
+		btnLogin.click();
+	}
+
+	public void clickproductlink() {
+		productlink.click();
+	}
 
 }
